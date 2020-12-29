7a19e4c7472d543b595e008cc9b70d2cc1800723
Christopher DiStasio <cadistasio@gmail.com>
2020-12-29 13:07:57 -0600

    Adds, starts python collections practice

---
+++ python-collections.py
@@ -0,0 +1,44 @@
+# Collections
+
+# Python has no "new" keyword to instantiate a built-in data type"
+
+a = list()
+b = tuple()
+c = range(6)
+d = set()
+e = dict()
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+"""
+[]
+()
+range(0, 6)
+set()
+{}
+"""
+
+#Lists
+
+#As an array in JS can, a List can contain multiple data types
+
+
+a.append("Bob")
+a.append(34)
+
+print(a)
+
+
+
+#Tuples
+
+
+#Sets
+
+
+
+#Dictionaries
