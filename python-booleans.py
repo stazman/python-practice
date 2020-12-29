78d203367dad5453f564d704b9352024bc2e1c66
Christopher DiStasio <cadistasio@gmail.com>
2020-12-29 13:06:04 -0600

    Adds, begins python boolean practice

---
+++ python-booleans.py
@@ -0,0 +1,20 @@
+# Booleans
+
+from typing import Tuple
+
+
+print(bool(True))
+print(bool(False))
+
+
+
+# Truthy and falsy values
+
+#Different than JavaScript
+
+print(bool([]))
+print(bool({}))
+
+
+
+
