310775929ba81b2606adcb570421db8a117a3a8b
Christopher DiStasio <cadistasio@gmail.com>
2021-01-05 12:20:12 -0600

    Adds collections categories, completes list info

--- python-collections.py
+++ python-collections.py
@@ -1,6 +1,6 @@
 # Collections
 
-# Python has no "new" keyword to instantiate a built-in data type"
+# Python has no "new" keyword to instantiate a built-in data type
 
 a = list()
 b = tuple()
@@ -24,17 +24,42 @@ set()
 
 #Lists
 
-#As an array in JS can, a List can contain multiple data types
+# As an array in JS can, a List can contain multiple data types
+
+# Note: There are some list methods that will change the order, but in general: the order of the items will not change.
+
+# Since lists are indexed, lists can have items with the same value
+
+# You can use the literal or list constructor to create a list. Note the double round brackets in hte constructor:
+
+l = [1,2,3,4]
+
+l2 = list(('cat', 3, True, None, True))
+
+for item in l:
+  print(item)
+
+for item in l2:
+  print(item)
 
 
-a.append("Bob")
-a.append(34)
+# Accessing list items
 
-print(a)
+print(l2[0])
 
+print(l2[-1])
+
+print(l2[0:2]) # not inclusive of 2
+
+print(l2[2:]) # not inclusive of 2
+
+print(l2[:2]) # not inclusive of 2
 
 
 #Tuples
+
+# A tuple is similar to an enum in Java. It is unchangeable.
+
 
 
 #Sets
