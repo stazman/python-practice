799f1ff42884acddc448d31e82a5e06a327f2459
Christopher DiStasio <cadistasio@gmail.com>
2021-01-07 22:40:19 -0600

    Begins basic tuple practice

--- python-collections.py
+++ python-collections.py
@@ -58,13 +58,36 @@ print(l2[:2]) # not inclusive of item at index 2
 
 #Tuples
 
-# A tuple is similar to an enum in Java. It is unchangeable.
+# A tuple is a collection which is ordered and unchangeable.
 
+# A tuple is similar to an enum in Java or objects passed through Object.freeze() in JavaScript, which are unchangeable.
 
+  # JavaScript ... const arr = [1,2,3,4]; Object.freeze(arr); arr.pop() will result in an error
+  # Java ... enum TestEnum = { YESTERDAY, TODAY, TOMORROW };
+
+# Since tuples are indexed, lists can have items with the same value
+
+# Tuples are used to store multiple items in a single variable.
+
+# Tuples are written with round brackets.
+
+# To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.
+
+test_tuple_one_item = ( "happy", )
+# OR
+test_tuple_one_item_constructor = tuple(("happy", ))
+# note double round brackets ... without them, it will turn each character in "happy" into an item
+
+print(test_tuple_one_item)
+print(test_tuple_one_item_constructor)
+
+print(len(test_tuple_one_item))
 
 
 
 #Sets
+
+
 
 
 
