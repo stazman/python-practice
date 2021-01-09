1a787fcfff6f923540399745f2f637716414cabb
Christopher DiStasio <cadistasio@gmail.com>
2021-01-08 20:16:19 -0600

    Adds, stubs debugging practice

---
+++ python-debugging.py
@@ -0,0 +1,6 @@
+#Debugging in Python: put this code where you want to set a breakpoint for the debugging. You would put it in the same place you would put debugger in Chrome Dev Console for JS:
+
+      # import pdb; pdb.set_trace()
+
+      # https://codeburst.io/how-i-use-python-debugger-to-fix-code-279f11f75866#:~:text=Python%20has%20a%20built%2Din,that%20does%20the%20main%20job.&text=As%20soon%20as%20the%20interpreter,but%20with%20some%20new%20commands
+
