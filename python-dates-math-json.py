d21a8c20f511580d8e708f3575a83d9958040e26
Christopher DiStasio <cadistasio@gmail.com>
2021-01-22 21:13:17 -0600

    Completes JSON in Python practice

--- python-dates-math-json.py
+++ python-dates-math-json.py
@@ -111,4 +111,75 @@ print(x["name"])
 
 # Converting from Python to JSON: If you have a Python object, you can convert it into a JSON string by using the json.dumps() method. (similar to the built-in object JSON's .stringify() method in JS)
 
+petobject = {
+  "name": "Cash",
+  "age": 2
+}
 
+pythontojson = json.dumps(petobject)
+print(pythontojson)
+print(type(pythontojson))
+
+# You can convert Python objects of the following types, into JSON strings:
+
+# dict
+# list
+# tuple
+# string
+# int
+# float
+# True
+# False
+# None
+
+# When you convert from Python to JSON, Python objects are converted into the JSON (JavaScript) equivalent:
+
+# Python	JSON
+# dict	Object
+# list	Array
+# tuple	Array
+# str	String
+# int	Number
+# float	Number
+# True	true
+# False	false
+# None	null
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
+
+# The json.dumps() method has parameters to make it easier to read the result. Use the indent parameter to define the numbers of indents:
+
+print("\nWith custom indenting: \n" + json.dumps(x, indent=4))
+
+
+# You can also define the separators, default value is (", ", ": "), which means using a comma and a space to separate each object, and a colon and a space to separate keys from values. You can make custom values, using the separators parameter to change the default separator.
+
+print("\nWith custom separators: \n" + json.dumps(x, indent=4, separators=(". ", " = ")))
+
+
+# The json.dumps() method has parameters to order the keys in the result. Use the sort_keys parameter to specify if the result should be sorted or not.
+
+print("\nWith ordering: \n" + json.dumps(x, indent=4, separators=(". ", " = "), sort_keys=True))
